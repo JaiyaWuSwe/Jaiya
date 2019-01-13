7d75b905029366729e267338ad5ffe2329270b10
Wichuta18 <slotsnow0301@gmail.com>
2019-01-13 12:21:50 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { LoginPage } from './../pages/login/login';
+// import { FirstpagePage } from './../pages/firstpage/firstpage';
+// import { LoginPage } from './../pages/login/login';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RegisterPage } from '../pages/register/register';
 
 // import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = RegisterPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
